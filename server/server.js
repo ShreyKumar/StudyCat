e023431799a5da578c2e8928a62568838b53e62b
hongyu-wang <hongyu.wang@mail.utoronto.ca>
2017-10-28 16:43:10 -0400

    basic server ecksdee

--- server/server.js
+++ server/server.js
@@ -2,3 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', function(req, res) {
+  res.send('Hello Seattle\n');
+});
+
+
+app.listen(3001);
+console.log('Listening on port 3001...');
