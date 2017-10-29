632977a70267003b2fd0ed1c0018855436fe20fd
hongyu-wang <hongyu.wang@mail.utoronto.ca>
2017-10-29 17:50:08 -0400

    Basic post server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,39 @@
-const express = require('express')
+const port = 3000;
+const express = require('express');
+const body_parser = require("body-parser");
+const app = express();
 
-const app = express()
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
 
-app.get('/', function(req, res) {
-  res.send('{"Test": "Test"}\n');
+
+app.post('/send_data', function(req, res) {
+  	res.send('{"Test": "Test"}\n');
 });
 
 
-app.listen(3001);
-console.log('Listening on port 3001...');
+
+app.post('/input_data', function(req, res) {
+	var user_name = req.body.user
+
+	var processes = req.body.processes
+
+	console.log(user_name)
+
+	console.log(processes)
+	var response_string = ""
+	if (!user_name)
+		response_string += "no user name"
+
+	if (!processes)
+		response_string += "processes invalid"
+	
+	if (response_string != "")
+		res.status(400).send(response_string)
+	else
+		res.status(200).send("successful")
+});
+
+
+app.listen(port);
+console.log('Listening on port '+port);
