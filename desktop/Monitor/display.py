53c7a81419d80e47821b7cdb485a3763e7acf471
KevinMKang <kevinmkang97@gmail.com>
2017-10-29 21:09:23 -0400

    process monitoring code

---
+++ desktop/Monitor/display.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter.filedialog import *
+from alt_processes import *
+class Application(Frame):
+	def __init__(self, productivityList, master=None):
+		super().__init__(master)
+		self.pack()
+		self.currEXE = StringVar()
+		self.currEXE.set("")
+		self.readList(productivityList)
+		self.category = StringVar(master)
+		self.category.set("Neutral")
+		self.categories = ["Very Unproductive", "Unproductive", "Neutral", "Productive", "Very Productive"]
+		self.create_widgets()
+		
+	def readList(self, file):
+		self.productivityList = []
+		
+		with open(file) as f:
+			lines = f.readlines()
+		
+		for line in lines:
+			self.productivityList.append(line.split(","))
+			
+		
+	def create_widgets(self):
+		self.label = Label(self, textvariable=self.currEXE)
+		self.label.pack(side="top")
+		
+		self.selectProgram = Button(self, text="Select Program", command=self.openFile)
+		self.selectProgram.pack(side="top")
+		
+		self.selectCategory = OptionMenu(self, self.category, *self.categories) 
+		self.selectCategory.pack(side="top")
+		
+		self.addProgram = Button(self, text="Add Program", command=self.addProgram)
+		self.addProgram.pack(side="top")
+		
+		self.start = Button(self,text="Start Monitoring", command=self.startMonitoring)
+		self.start.pack(side="top")
+		
+	def addProgram(self):
+		
+		self.productivityList[self.categories.index(self.category.get())].append(self.currEXE.get())
+		print(self.productivityList)
+		self.currEXE.set("No Program Selected")
+		
+		
+	def openFile(self):
+		exe = askopenfilename().split("/")
+		self.currEXE.set(exe[len(exe)-1])
+		print(self.currEXE)
+		
+	def startMonitoring(self):
+		with open("updatedPLists.txt", "w") as f:
+			for list in self.productivityList:
+				f.write(",".join(str(x) for x in list))
+		
+		startMonitoring(self.productivityList)
+		
+
+if __name__ == "__main__":
+	file = "./pList.txt"
+	root = Tk()
+	app = Application(file, master=root)
+	app.master.geometry("400x300")
+	app.mainloop()
+
