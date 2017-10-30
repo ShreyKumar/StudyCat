de03a61cef39614b1fe456a69192cb04d82d5ba8
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2017-10-29 22:01:15 -0400

    allowed cors request

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ const app = express()
 
 app.use(body_parser.urlencoded({ extended: false }))
 app.use(body_parser.json())
+
+//cors request
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+})
+
 app.users = []
 app.user_to_data = {}
 
