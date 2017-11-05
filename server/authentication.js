35d41689c2f525a874967ea187ad36872c5d4cd5
kekuraz <jeff.lin.97@gmail.com>
2017-11-05 00:00:12 -0400

    added auth stuff

---
+++ server/authentication.js
@@ -0,0 +1,61 @@
+    
+var firebase = require("firebase");
+ 
+function signUp(email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
+
+function signIn(femail, password) {
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  
+  if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+  });
+}
+
+function signOut() {
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+  }).catch(function(error) {
+  // An error happened.
+  });
+}
+
+//Update App
+function signInListener() {
+  // Listening for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+
+      } else {
+      
+      }
+
+  });
+
+}
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,21 @@
-module.exports = function(app){
-    var user = require('./controllers/users');
-    app.get('/users/:id', users.find);
-    app.post('/users', users.add);
-    app.put('/users/:id', users.update);
-    app.delete('/users/:id', users.delete);
-}
+module.exports = function(app, auth, email, password) {
+
+    // route for logging out
+    app.get('/logout', auth.signOut());
+
+    app.get('/login', function(req, res) {
+        if (email) { //TODO add code to check if user exists
+
+            auth.signUp(email, password);
+            res.status(200).send("Successfully created new user and signed in.")
+        } else if (email && password) {
+            auth.signIn(email, password);
+            res.status(200).send("Successfully signed in.")
+        } else {
+            res.status(400).send("wot")
+        }
+        
+        app.redirect('/');
+    });
+
+};
