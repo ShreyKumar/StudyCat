939c2c032cf041596cf37b8d29122f0d8b729212
KevinMKang <kevinmkang97@gmail.com>
2017-11-06 20:06:40 -0500

    split up main Tkinter root and GUI display

--- desktop/gui/DesktopApp.py
+++ desktop/gui/DesktopApp.py
@@ -10,31 +10,17 @@ from time import sleep
 
 from threading import Thread
 
-class DesktopApp:
-    def __init__(self):
-        self.root = Tk()
-        self.cat = Cat()
-
-        #self.root.overrideredirect(1)
-
+class CatDisplay:
+    def __init__(self, master, cat):
+        self.root = master
+        self.cat = cat
         img = ImageTk.PhotoImage(self.cat.getImage().resize((250, 250), Image.ANTIALIAS))
         self.panel = Label(self.root, image=img)
         self.panel.pack(side="bottom", fill="both", expand="yes")
 
         self.label = Label(self.root, text="Happiness: " + str(self.cat.getState()), fg=self.cat.getText())
         self.label.pack(side="bottom", fill="both", expand="yes")
-
         self.update()
-        #self.updateLoop(-1)
-
-        self.monitor = monitor.Monitor("./../Monitor/pList.txt")
-        self.running = 1
-        self.thread1 = Thread(target=self.monitorProcesses)
-        self.thread1.start()
-
-        self.updateAffection()
-
-        self.root.mainloop()
 
     def update(self):
         img = ImageTk.PhotoImage(self.cat.getImage().resize((250, 250), Image.ANTIALIAS))
@@ -48,25 +34,25 @@ class DesktopApp:
 
         self.root.after(1000, self.update)
 
-    # temp function for updating the cat's state
-    def updateCat(self, change):
-        state = self.cat.getState()
-        state += change
-        if state == 0:
-            state = 0
-        if state > 99:
-            state = 99
-        self.cat.setState(state)
 
-    def updateLoop(self, change):
-        if self.cat.getState() == 0:
-            change = 1
-        if self.cat.getState() == 99:
-            change = -1
 
-        self.updateCat(change)
+class DesktopApp:
+    def __init__(self):
+        self.root = Tk()
+        self.cat = Cat()
 
-        self.root.after(100, self.updateLoop, change)
+        self.GUI = CatDisplay(self.root, self.cat)
+
+        #self.root.overrideredirect(1)
+
+        self.monitor = monitor.Monitor("./../Monitor/pList.txt")
+        self.running = 1
+        self.thread1 = Thread(target=self.monitorProcesses)
+        self.thread1.start()
+
+        self.updateAffection()
+
+        self.root.mainloop()
 
     def monitorProcesses(self):
         while self.running:
