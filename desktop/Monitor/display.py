23fcc573834e8f1189b2665dc5046555e362f35b
KevinMKang <kevinmkang97@gmail.com>
2017-11-16 22:51:31 -0500

    styled settings screen

--- desktop/Monitor/display.py
+++ desktop/Monitor/display.py
@@ -1,9 +1,11 @@
-from tkinter import *
+#from tkFont import Font
 from PIL import ImageTk, Image
 from tkinter.filedialog import *
 
 white = "#ffffff"
-
+myFont = "Roboto 10"
+style = {"font":myFont,"relief":"flat", "bg":"#ff6262", "fg":"#ffffff"}
+pady=5
 
 class programDisplay(Frame):
 
@@ -16,7 +18,7 @@ class programDisplay(Frame):
 class MainFrame(Frame):
 
     def __init__(self, master, productivityList, startCommand):
-        Frame.__init__(self, master,bg="#FFB6C1")
+        Frame.__init__(self, master,bg="#FFFFFF")
         self.startCommand = startCommand
         self.currEXE = StringVar()
         self.currEXE.set("No Program Selected")
@@ -28,8 +30,7 @@ class MainFrame(Frame):
         self.categories = ["Very Unproductive", "Unproductive", "Neutral", "Productive", "Very Productive"]
 
         self.programDisplayText = StringVar(master)
-        self.programDisplayText.set("\n".join(self.productivityList[self.categories.index(self.category.get())]))
-
+        self.updateDisplayedPrograms(None)
         self.create_widgets()
 
     def readList(self, file):
@@ -42,34 +43,40 @@ class MainFrame(Frame):
 
     def create_widgets(self):
 
-        self.title = Label(self, text="Initialization", font=("Helvetica", 16),bg="#FFB6C1")
-        self.title.pack()
+        self.title = Label(self, font="Roboto 16 bold", text="Initialization", bg=white)
+        self.title.pack(pady=pady)
 
-        self.label = Label(self, textvariable=self.currEXE,bg="#FFB6C1")
-        self.label.pack(side="top")
+        self.label = Label(self, font=myFont, textvariable=self.currEXE, bg=white,pady=pady)
+        self.label.pack(side="top",pady=pady)
 
-        self.selectProgram = Button(self, text="Select Program", command=self.openFile,bg=white)
-        self.selectProgram.pack(side="top")
+        self.selectProgram = Button(self, text="Select Program", command=self.openFile)
+        self.selectProgram.configure(style)
+        self.selectProgram.pack(side="top",pady=pady)
 
         self.selectCategory = OptionMenu(self, self.category, *self.categories, command=self.updateDisplayedPrograms)
-        self.selectCategory.config(bg=white)
-        self.selectCategory.pack(side="top")
+        self.selectCategory.configure(style)
+        self.selectCategory.pack(side="top",pady=pady)
 
         self.addProgram = Button(self, text="Add Program", command=self.addProgram,bg=white)
-        self.addProgram.pack(side="top")
+        self.addProgram.configure(style)
+        self.addProgram.pack(side="top",pady=pady)
 
         self.startMonitoring = Button(self,text="Start",
                                       command=self.startCommand,bg=white)
-        self.startMonitoring.pack(side="top")
+        self.startMonitoring.configure(style)
+        self.startMonitoring.pack(side="top",pady=pady)
 
-        self.programDisplay = Label(self, textvariable=self.programDisplayText,bg=white,highlightbackground="#000000",pady=20)
-        self.programDisplay.pack()
+
+
+        self.programDisplay = Label(self, textvariable=self.programDisplayText,bg=white, borderwidth=1)
+        self.programDisplay.configure(font=myFont, relief="groove", pady=20)
+
+        self.programDisplay.pack(pady=pady)
 
     def updateDisplayedPrograms(self, event):
-        self.programDisplayText.set("\n".join(self.productivityList[self.categories.index(self.category.get())]))
+        self.programDisplayText.set(self.category.get() + " Programs\n" + "\n".join(self.productivityList[self.categories.index(self.category.get())]))
 
     def addProgram(self):
-
         if self.currEXE.get() != "No Program Selected":
             self.productivityList[self.categories.index(self.category.get())].append(self.currEXE.get())
             print(self.productivityList)
@@ -78,7 +85,9 @@ class MainFrame(Frame):
 
     def openFile(self):
         exe = askopenfilename().split("/")
-        self.currEXE.set(exe[len(exe)-1])
+
+        if exe[0] != '':
+            self.currEXE.set(exe[len(exe)-1])
 
     #def startMonitoring(self):
     #    with open("updatedPLists.txt", "w") as f:
