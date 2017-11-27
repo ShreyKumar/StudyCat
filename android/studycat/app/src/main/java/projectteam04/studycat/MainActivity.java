7459b3efc77100e5241ae921ab8f8cbd68d987b6
darwin <darwin.truong@outlook.com>
2017-11-27 17:25:19 -0500

    asdf

--- android/studycat/app/src/main/java/projectteam04/studycat/MainActivity.java
+++ android/studycat/app/src/main/java/projectteam04/studycat/MainActivity.java
@@ -1,13 +1,29 @@
 package projectteam04.studycat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    NotificationManager nManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showNotification();
+    }
+
+    private void showNotification() {
+        nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification n = new Notification.Builder(this)
+                .setContentTitle("Hi")
+                .setContentText("notification btw")
+                .setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        nManager.notify(0, n);
     }
 }
