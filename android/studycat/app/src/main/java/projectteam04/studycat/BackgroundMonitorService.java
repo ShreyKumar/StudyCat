3177fddfbb6a6dd4bee15d79aee1df4c7e56b27f
darwin <darwin.truong@outlook.com>
2017-11-28 00:06:48 -0500

    added background monitor skeleton

---
+++ android/studycat/app/src/main/java/projectteam04/studycat/BackgroundMonitorService.java
@@ -0,0 +1,24 @@
+package projectteam04.studycat;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by darwin on 2017-11-27.
+ */
+
+public class BackgroundMonitorService extends IntentService {
+
+    public BackgroundMonitorService() {
+        super("BackgroundMonitorService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+        
+
+    }
+}
--- android/studycat/app/src/main/java/projectteam04/studycat/MainActivity.java
+++ android/studycat/app/src/main/java/projectteam04/studycat/MainActivity.java
@@ -2,6 +2,8 @@ package projectteam04.studycat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,30 +11,54 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int NOTIFICATION_ID = 0;
     NotificationManager nManager;
+    Notification.Builder notifBuilder;
+    Intent monitorService;
     Button btn;
+    int c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.button);
+        c = 0;
+        notifBuilder = new Notification.Builder(this);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showNotification();
+                updateNotification();
+                c++;
             }
         });
+
+        // Set up the background process
+        monitorService = new Intent(this, BackgroundMonitorService.class);
+        this.startService(monitorService);
     }
 
     private void showNotification() {
         nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Notification n = new Notification.Builder(this)
+        Notification n = notifBuilder
                 .setContentTitle("Hi")
                 .setContentText("notification btw")
                 .setOngoing(true)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .build();
-        nManager.notify(0, n);
+        nManager.notify(NOTIFICATION_ID, n);
+    }
+
+    private void updateNotification() {
+
+        if (nManager == null) {
+            showNotification();
+        }
+
+        notifBuilder.setContentText(Integer.toString(c));
+
+        nManager.notify(NOTIFICATION_ID,
+                notifBuilder.build());
     }
 }
